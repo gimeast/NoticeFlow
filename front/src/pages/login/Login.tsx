3dba9c0b4de66b0ccebe7d5bd5b952c527fe6d0f
gimeast <slasnrndu@gmail.com>
2026-01-02 00:22:31 +0900

    NoticeFlow 프론트
    - 구글 로그인 연결

--- front/src/pages/login/Login.tsx
+++ front/src/pages/login/Login.tsx
@@ -4,6 +4,9 @@ import Button from '@/components/buttons/Button.tsx';
 import style from './Login.module.scss';
 
 const Login = () => {
+    const handleLogin = () => {
+        window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/google`;
+    };
     return (
         <div className={style.container}>
             <h1 className='sr-only'>NoticeFlow 로그인</h1>
@@ -11,7 +14,7 @@ const Login = () => {
                 <LogoIcon />
                 <strong>NoticeFlow</strong>
                 <p className={style.about}>공지 전달 시스템</p>
-                <Button type='button' size='full' border={1}>
+                <Button type='button' size='full' border={1} onClick={handleLogin}>
                     <GoogleIcon />
                     Google 계정으로 로그인
                 </Button>
