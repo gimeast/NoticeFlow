23ccebda4d945c1266fa30907e49dbb17d23ecf7
gimeast <slasnrndu@gmail.com>
2026-01-03 01:23:43 +0900

    NoticeFlow 프론트
    - OAuth2 리다이렉트 핸들러에서 `useEffect` 내부로 사용자 정보 조회 로직 이동 및 의존성 배열 업데이트

--- front/src/pages/oauth/OAuth2RedirectHandler.tsx
+++ front/src/pages/oauth/OAuth2RedirectHandler.tsx
@@ -7,31 +7,31 @@ const OAuth2RedirectHandler = () => {
     const { login } = authStore();
     const navigate = useNavigate();
 
-    const getUser = async () => {
-        try {
-            const res = await fetch(USER, {
-                method: 'GET',
-                credentials: 'include',
-            });
-
-            if (!res.ok) throw new Error('사용자 정보 조회 중 에러 발생');
-            const { data } = await res.json();
-
-            login(data.email, data.name, data.role);
-
-            if (data.status === 'PENDING') {
-                navigate('/join');
-            } else {
-                //TODO: 기관사용자, 일반사용자 분기처리 필요
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
     useEffect(() => {
+        const getUser = async () => {
+            try {
+                const res = await fetch(USER, {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+
+                if (!res.ok) throw new Error('사용자 정보 조회 중 에러 발생');
+                const { data } = await res.json();
+
+                login(data.email, data.name, data.role);
+
+                if (data.status === 'PENDING') {
+                    navigate('/join');
+                } else {
+                    //TODO: 기관사용자, 일반사용자 분기처리 필요
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
         getUser();
-    }, []);
+    }, [login, navigate]);
     return <div>로그인 처리 중...</div>;
 };
 
