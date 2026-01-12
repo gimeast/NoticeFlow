79329471f86f14aa323a9ffac8d00336cf237577
gimeast <slasnrndu@gmail.com>
2026-01-12 11:06:31 +0900

    NoticeFlow 프론트
    - 수정: 초반엔 서버 플러그인 사용을 고려하여 use server를 명시하였지만 필요없어서 제거함

--- front/src/actions/actions.ts
+++ front/src/actions/actions.ts
@@ -1,5 +1,3 @@
-'use server';
-
 import { apiClient } from '@/api/client.ts';
 import { ORGANIZATION_REGISTER } from '@/api/endpoints.ts';
 import type { RoleType, StatusType } from '@/types/authTypes.ts';
