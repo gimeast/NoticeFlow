5b145e5b53ac1657cfe32bacd86072527a17c606
gimeast <slasnrndu@gmail.com>
2026-01-14 15:38:25 +0900

    NoticeFlow 프론트
    - 수정: 대시보드 레이아웃 스타일 정리 및 noticeContainer 크기 조정

--- front/src/layouts/dashboard/DashboardLayout.tsx
+++ front/src/layouts/dashboard/DashboardLayout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = () => {
             <Header />
             {role === 'ORGANIZATION' && <OrganizationSidebar />}
             {role === 'NORMAL' && <NormalSidebar />}
-            <main className={style.main}>
+            <main>
                 <Outlet />
             </main>
         </div>
